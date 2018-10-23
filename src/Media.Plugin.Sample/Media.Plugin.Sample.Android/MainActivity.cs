555e119cc19a3486e55b4934d54527a27d3821d6
Luke Lau <luke.lau@theappbuilder.com>
2018-10-23 01:27:15 +0100

    Setup permissions for android sample

--- src/Media.Plugin.Sample/Media.Plugin.Sample.Android/MainActivity.cs
+++ src/Media.Plugin.Sample/Media.Plugin.Sample.Android/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
 
 namespace Media.Plugin.Sample.Droid
 {
@@ -19,9 +21,16 @@ namespace Media.Plugin.Sample.Droid
 
             base.OnCreate(bundle);
 
-            global::Xamarin.Forms.Forms.Init(this, bundle);
+			CrossCurrentActivity.Current.Init(this, bundle);
+
+			global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
-    }
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Android.Content.PM.Permission[] grantResults)
+		{
+			PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+		}
+	}
 }
 
